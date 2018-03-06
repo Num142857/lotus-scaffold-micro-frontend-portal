62d7129aab0d14f7fc2e728e17180b4abebfc8e5
杨帆 <incomparable9527@foxmail.com>
2018-03-06 17:26:34 +0800

    init

---
+++ src/Portal.js
@@ -0,0 +1,21 @@
+import * as singleSpa from 'single-spa'; // waiting for this to be merged: https://github.com/CanopyTax/single-spa/pull/156
+import { GlobalEventDistributor } from './GlobalEventDistributor'
+import { importApp } from './Util'
+
+(async function () {
+    const globalEventDistributor = new GlobalEventDistributor();
+
+    // app1: The URL "/app1/..." is being rewritten to "http://localhost:9001/..." this is done by the webpack proxy
+    await importApp('app1', '/app1', '/app1/main.js', '/app1/store.js', globalEventDistributor);
+
+    // app2: The URL "/app2/..." is being rewritten to "http://localhost:9002/..." this is done by the webpack proxy
+    await importApp('app2', '/app2', '/app2/main.js', '/app2/store.js', globalEventDistributor);
+
+    // app3: The URL "/app3/..." is being rewritten to "http://localhost:9003/..." this is done by the webpack proxy
+    await importApp('app3', '/app3', '/app3/main.js', null, null); // does not have a store, so we pass null
+
+    singleSpa.start();
+})()
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: {
+		main: 'src/Portal.js',
+	},
+	output: {
+		publicPath: '',
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'release'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				exclude: [path.resolve(__dirname, 'node_modules')],
+				loader: 'babel-loader',
+			}
+		],
+	},
+	node: {
+		fs: 'empty'
+	},
+	resolve: {
+		modules: [
+			__dirname,
+			'node_modules',
+		],
+	},
+	plugins: [
+        CopyWebpackPlugin([
+            {from: path.resolve(__dirname, 'src/index.html')},
+            {from: path.resolve(__dirname, 'libs/system.js')},
+        ]),
+		new CleanWebpackPlugin(['release'])
+	],
+	devtool: 'source-map',
+	externals: [
+	],
+    devServer: {
+		contentBase: './release',
+        historyApiFallback: true,
+        watchOptions: { aggregateTimeout: 300, poll: 1000 },
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        },
+        proxy: {
+            "/app1": {
+                target: "http://localhost:9001",
+                pathRewrite: {"^/app1" : ""}
+            },
+            "/app2": {
+                target: "http://localhost:9002",
+                pathRewrite: {"^/app2" : ""}
+            },
+            "/app3": {
+                target: "http://localhost:9003",
+                pathRewrite: {"^/app3" : ""}
+            }
+        }
+    },
+};
