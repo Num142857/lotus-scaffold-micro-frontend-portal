0f01c78f69c0a29e75ac8249a2b620651300ed21
杨帆 <incomparable9527@foxmail.com>
2018-04-26 11:21:34 +0800

    test

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -1,7 +1,7 @@
 "use strict";
 import * as singleSpa from 'single-spa'; 
 import { registerApp } from './Register'
-import 'babel-polyfill';
+
 async function bootstrap() {
     let projectConfig = await SystemJS.import('/project.js')
     projectConfig.projects.forEach( async element => {
