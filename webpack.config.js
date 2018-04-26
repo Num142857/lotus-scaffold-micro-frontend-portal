92197bb024507f646e296eff0de1e1025e04ddb0
杨帆 <incomparable9527@foxmail.com>
2018-04-26 11:37:22 +0800

    优化

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = {
             {from: path.resolve(__dirname, 'src/index.html')},
 			// { from: path.resolve(__dirname, 'src/project.js')},
             {from: path.resolve(__dirname, 'libs/system.js')},
+			{ from: path.resolve(__dirname, 'libs/es6-promise.auto.min.js')},
         ]),
 		new CleanWebpackPlugin(['build'])
 	],
