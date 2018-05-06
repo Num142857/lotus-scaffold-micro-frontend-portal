9aa0814c8f5b2802dcb4350aeaeeeacc7a7ad0fb
Fan <incomparable9527@foxmail.com>
2018-05-06 13:47:00 +0800

    fixed

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -5,8 +5,8 @@ import { registerApp } from './Register'
 
 async function bootstrap() {
     let projectConfig = await SystemJS.import('/project.js')
-    projectConfig.projects.forEach( async element => {
-        await registerApp({
+    projectConfig.projects.forEach( element => {
+        registerApp({
             name: element.name,
             main: element.main,
             url: element.prefix,
